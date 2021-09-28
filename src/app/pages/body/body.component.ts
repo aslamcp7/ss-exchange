413885c2119a525d97027f1ee8543eeb093b649b
Matajar <info.matajar@gmail.com>
2021-09-29 01:26:15 +0400

    body table completed

--- src/app/pages/body/body.component.ts
+++ src/app/pages/body/body.component.ts
@@ -7,16 +7,40 @@ import { Component, OnInit } from "@angular/core";
 })
 export class BodyComponent implements OnInit {
   elements: any = [];
-  headElements = ["id", "first", "last", "handle", "Aslam"];
+  headElements = [
+    "Application Name",
+    "User Name",
+    "Net Balance",
+    "Reg. Date ",
+    "Status",
+    "Balance in Company Currency",
+    "Balance",
+    "MSA Total Balance",
+    "Super Total Balance",
+    "Credit",
+    "Setting Balance",
+    "Deal Currency",
+    "Deal Rate",
+    "Balance in Deal Currency",
+  ];
 
   ngOnInit() {
-    for (let i = 1; i <= 15; i++) {
+    for (let i = 1; i <= 8; i++) {
       this.elements.push({
-        id: i,
-        first: "User " + i,
-        last: "Name " + i,
-        handle: "Handle " + i,
-        aslam: "aslam " + i,
+        ApplicationName: "Mary Dona",
+        UserName: "Mary Dona",
+        NetBalance: "INR ",
+        RegDate: "30/10/2020",
+        Status: "ACTIVE",
+        BalanceinCompanyCurrency: "107,41.90",
+        Balance: "0.00",
+        MSATotalBalance: "0.00",
+        SuperTotalBalance: "107,41.90",
+        Credit: "0.00",
+        SettingBalance: "107,41.90",
+        DealCurrency: "INR",
+        DealRate: "10%",
+        BalanceinDealCurrency: "0.00",
       });
     }
   }
