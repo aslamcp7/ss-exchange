547307caff2bd2cbfa01328a01771f9097e7ba76
Matajar <info.matajar@gmail.com>
2021-09-29 17:59:04 +0400

    modification

--- src/app/pages/body/body.component.ts
+++ src/app/pages/body/body.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from "@angular/core";
 export class BodyComponent implements OnInit {
   elements: any = [];
   status: boolean = false;
+  visible: false;
   headElements = [
     "Application Name",
     "User Name",
